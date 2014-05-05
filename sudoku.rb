3e2a7d5c04d50d148edc7402c55813d84cf045c3
Sroop <sroopsunar@hotmail.com>
2014-05-05 15:45:14 +0100

    reverted back to an old crappy version to generate levels. zeros aren't evenly distributed but for the sake of having working buttons, will go with this verion. recursion and while loops are commented out in the main rb file

--- sudoku.rb
+++ sudoku.rb
@@ -20,25 +20,68 @@ def random_sudoku
   sudoku.to_s.chars
 end
 
-def contains_nine_zeros?(array)
-  newarray = array.select {|number| number == "0"}
-  newarray.count == 9
-end
+# RECURSIVE METHOD.FINE ADDING O'S IN ROWS AND COLS EVENLY, BUT STACK LEVEL
+# IS TOO DEEP WHEN I CROSS REFERENCE IT WITH BOXES. 
+# FORMAULA TO FIND BOX INDEX GIVEN A RANDOM COORDAINTE IS CORRECT
 
-def puzzle(sudoku, level=3)
-  kinda_empty_sudoku = sudoku.dup
-  until contains_nine_zeros?(kinda_empty_sudoku) do
-    kinda_empty_sudoku = sudoku.dup
-    boxes = boxes(kinda_empty_sudoku).each{|box|
-        box[rand(9)]="0" if !box.include?("0") }
-    rows = rows(boxes.flatten).each {|row|
-        row[rand(9)]="0" if !row.include?("0")}
-    cols = columns(rows.flatten).each {|col|
-        col[rand(9)]="0" if !col.include?("0")}
-    kinda_empty_sudoku = cols.transpose.flatten
-  end
-  boxes(kinda_empty_sudoku).flatten
-end
+# def contains_nine_zeros?(array)
+#   newarray = array.select {|number| number == "0"}
+#   newarray.count == 9
+# end
+
+# def puzzle(sudoku, level=9)
+#   kinda_empty_sudoku = sudoku.dup
+#   kinda_empty_sudoku = rows(kinda_empty_sudoku)
+#   kinda_empty_sudoku[rand(9)][rand(9)] = "0"
+#   place_nine_zeros(kinda_empty_sudoku)
+# end
+
+# def place_zero(kinda_empty_sudoku, next_cell)
+#   x, y = next_cell
+#   columns_array = kinda_empty_sudoku.transpose
+#   boxes_array = boxes(kinda_empty_sudoku.flatten)
+#     if !kinda_empty_sudoku[x].include?("0") && !columns_array[y].include?("0") && !boxes_array[(x/3)*3 + (y/3)].include?("0")
+#       kinda_empty_sudoku[x][y] = "0" 
+#       return kinda_empty_sudoku
+#     else 
+#       place_zero(kinda_empty_sudoku, [rand(9), rand(9)])
+#     end
+# end
+
+# def place_nine_zeros(kinda_empty_sudoku)
+#   next_cell = [rand(9),rand(9)]
+#   place_zero(kinda_empty_sudoku, next_cell)
+#   if contains_nine_zeros?(kinda_empty_sudoku.flatten)
+#     return kinda_empty_sudoku.flatten
+#   else
+#     place_nine_zeros(kinda_empty_sudoku)
+#   end
+# end
+
+# END OF RECURSIVE METHODS. 
+# BELOW USES WHILE LOOP AND WORKS OK FOR EVENLY PLACING 9 ZEROS BUT LONG LOAD TIME.
+
+# def contains_nine_zeros?(array)
+#   newarray = array.select {|number| number == "0"}
+#   newarray.count == 9
+# end
+
+# def puzzle(sudoku, level=3)
+#   kinda_empty_sudoku = sudoku.dup
+#   until contains_nine_zeros?(kinda_empty_sudoku) do
+#     kinda_empty_sudoku = sudoku.dup
+#     boxes = boxes(kinda_empty_sudoku).each{|box|
+#         box[rand(9)]="0" if !box.include?("0") }
+#     rows = rows(boxes.flatten).each {|row|
+#         row[rand(9)]="0" if !row.include?("0")}
+#     cols = columns(rows.flatten).each {|col|
+#         col[rand(9)]="0" if !col.include?("0")}
+#     kinda_empty_sudoku = cols.transpose.flatten
+#   end
+#   boxes(kinda_empty_sudoku).flatten
+# end
+
+# END OF WHILE LOOP
 
 def box_order_to_row_order(sudoku)
     boxes = boxes(sudoku)
@@ -65,12 +108,22 @@ def boxes(sudoku)
   sudoku.each_slice(9).to_a
 end
 
-get '/' do # default route for our website
+get '/' do
   prepare_to_check_solution
   generate_new_puzzle_if_necessary
   set_session_variables
   erb :index
 end
+
+# RANDOMLY PLACING ZEROS
+
+def puzzle(sudoku, level=14)
+  kinda_empty_sudoku = sudoku.dup
+  level.times {kinda_empty_sudoku[rand(81)]="0"}
+  kinda_empty_sudoku
+end
+
+# END OF RANDOMLY PLACING ZEROS
 
 def level_generator(level)
   session.clear()
@@ -83,15 +136,15 @@ def level_generator(level)
 end
 
 post '/easy' do
-  level_generator(2)
+  level_generator(14)
 end
 
 post '/medium' do
-  level_generator(40)
+  level_generator(35)
 end
 
 post '/hard' do
-  level_generator(100)
+  level_generator(75)
 end
 
 post '/restart' do
